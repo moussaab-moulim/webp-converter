25e0efd041ab6b4429431dd40698a3cca15fd759
moussaab moulim <42440629+moussaab-moulim@users.noreply.github.com>
2024-07-10 01:31:09 +0200

    remove global

--- install-webp.sh
+++ install-webp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 corepack enable
-yarn set version berry --global
+yarn set version berry
 
 # Install WebP
 curl -s https://raw.githubusercontent.com/Intervox/node-webp/latest/bin/install_webp | bash
